b6b3d7d6cde1e1fb9f482639d1294b963dae773b
OttoDragon <103608368+OttoDragon@users.noreply.github.com>
2023-10-16 17:14:37 -0300

    Add files via upload

---
+++ c-116-main/PRO_1-1_C116_TemplateDoProjeto-main/family tree/app.py
@@ -0,0 +1,44 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "rafael" # escreva seu nome
+    idade = "14" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+@app.route("/pai")
+def home():
+
+    nome = "guilherme" 
+    idade = "51" 
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página da mãe
+
+@app.route("/mae")
+def home():
+
+    nome = "nicole" 
+    idade = "47" 
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+
+# adicione outras rotas, se você quiser
+
+
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
